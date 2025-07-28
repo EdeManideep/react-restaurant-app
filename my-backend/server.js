3decdb0b0d56611507432702c0668875a7aeb402
EDE MANIDEEP <manideepede9@gmail.com>
2025-07-28 18:31:43 +0530

    Changed the DB password

--- my-backend/server.js
+++ my-backend/server.js
@@ -11,7 +11,7 @@ const pool = new Pool({
     user: 'manideep',
     host: 'react-food-app-items-cluster-16204.8nj.gcp-europe-west1.cockroachlabs.cloud',
     database: 'react_food_app_db',
-    password: 'oYso1hGEOUL2WlUP2O95wA',
+    password: 'jfB-11w1eTLfrQsBXMU4-g',
     port: 26257,
     ssl: {
         rejectUnauthorized: false, // Set to true in production for security
